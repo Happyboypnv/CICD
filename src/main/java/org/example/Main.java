dec432031ff19e1c9fa92863b887c615b35a2bde
Nguyen Van Phong <phongcho060207@gmail.com>
2026-05-05 10:49:11 +0700

    Bai08

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Chuong trinh Calculator dang chay!");
+        System.out.println("Ket qua 1 + 1 = " + calc.add(1, 1));
+    }
+}
